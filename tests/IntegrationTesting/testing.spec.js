26ef86daae0aad0a2c2926a1af1891046ec140a0
Likitha <likithatatineni7@gmail.com>
2026-06-18 21:22:16 +0530

    added new test for demo

---
+++ tests/IntegrationTesting/testing.spec.js
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('Launch The Browser', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await expect(page).toHaveTitle('Swag Labs');
+  await expect(page).toHaveURL('https://www.saucedemo.com/');
+
+  const logo = page.locator('.login_logo');
+  await expect(logo).toBeVisible();
+
+  const username = page.locator('#user-name');
+  await expect(username).toBeEnabled();
+
+  const userNames = page.locator('div[id="login_credentials"] h4');
+  await expect(userNames).toHaveText('Accepted usernames are:');
+
+  const attributeValue = page.locator('#user-name');
+  await expect(attributeValue).toHaveAttribute('placeholder', 'Username');
+  await expect(attributeValue).toHaveClass('input_error form_input');
+});

884a638b4d142c66f6543fd90120aeed570849b9
Likitha <likithatatineni7@gmail.com>
2026-06-24 00:29:47 +0530

    Add Test Case 2

--- tests/IntegrationTesting/testing.spec.js
+++ tests/IntegrationTesting/testing.spec.js
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('Launch The Browser', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
   await expect(page).toHaveTitle('Swag Labs');
-  await expect(page).toHaveURL('https://www.saucedemo.com/Engineer A');
+  await expect(page).toHaveURL('https://www.saucedemo.com/');
 
   const logo = page.locator('.login_logo');
   await expect(logo).toBeVisible();
@@ -57,4 +57,37 @@ test('Launch The Browser 23', async ({ page }) => {
   await expect(attributeValue).toHaveAttribute('placeholder', 'Username');
   await expect(attributeValue).toHaveClass('input_error form_input');
 });
+
+
+test('Test Case 1', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await expect(page).toHaveTitle('Swag Labs');
+  await expect(page).toHaveURL('https://www.saucedemo.com/');
+
+  const logo = page.locator('.login_logo');
+  await expect(logo).toBeVisible();
+
+  const username = page.locator('#user-name');
+  await expect(username).toBeEnabled();
+
+  const userNames = page.locator('div[id="login_credentials"] h4');
+  await expect(userNames).toHaveText('Accepted usernames are:');
+
+  const attributeValue = page.locator('#user-name');
+  await expect(attributeValue).toHaveAttribute('placeholder', 'Username');
+  await expect(attributeValue).toHaveClass('input_error form_input');
+});
+
+test('Test Case 2', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await expect(page).toHaveTitle('Swag Labs');
+  await expect(page).toHaveURL('https://www.saucedemo.com/');
+})
+
+test('Test Case 3', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await expect(page).toHaveTitle('Swag Labs');
+  await expect(page).toHaveURL('https://www.saucedemo.com/');
+})
+
 
